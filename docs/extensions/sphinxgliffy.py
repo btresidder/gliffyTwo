79f109f55436b5c03847c3c2e54c754c87f77001
btresidder <btresidder@dxc.com>
2020-09-23 09:09:12 +0930

    duplication issue 22

--- docs/extensions/sphinxgliffy.py
+++ docs/extensions/sphinxgliffy.py
@@ -31,7 +31,7 @@ class Sphinxgliffy(Directive):
         print(options)
         # Creates the svg class
         html_node = None
-        html_node = gliffy()
+        #html_node = gliffy()
         
         # Adds html class "gliffy_img" to all nodes created from now on
         self.options['classes'] = ['gliffy_img']
