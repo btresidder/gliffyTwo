ca5b501d3fcf2d95f44f03a9be1e7671ce96118f
btresidder <btresidder@dxc.com>
2020-09-22 14:54:24 +0930

    duplication issue 6

--- docs/extensions/sphinxgliffy.py
+++ docs/extensions/sphinxgliffy.py
@@ -49,12 +49,12 @@ def visit_gliffy(self, node):
     code = """<object data='_"""
     code += options['uri']
     code += """' type='image/svg+xml'></object>"""
-    options['uri'] = None
+    
     # Adds the <object> to the 
     self.body.append(code)
 
 def depart_gliffy(self, node):
-    pass
+    raise nodes.SkipNode
 
 # Setups up directives and nodes
 def setup(app):
