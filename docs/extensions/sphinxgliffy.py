373a2f00f3c1d60585854e8364a87fdcec5be28a
btresidder <btresidder@dxc.com>
2020-09-22 13:23:50 +0930

    complete working version on sphinxgliffy

--- docs/extensions/sphinxgliffy.py
+++ docs/extensions/sphinxgliffy.py
@@ -35,6 +35,7 @@ class Sphinxgliffy(Directive):
         
         # Creates an img version of the svg
         # Necessary to pass X Frame Options denial
+        # Image is not visible in browser, hidden by gliffy_img class
         html_node += nodes.image(rawsource=self.block_text, **self.options)
         
         return [html_node]
