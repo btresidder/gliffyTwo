6a8e9b9df16b3442491d1900d3fd804c0b41c222
btresidder <btresidder@dxc.com>
2020-09-23 13:44:30 +0930

    folder testing 16

--- docs/extensions/sphinxgliffy.py
+++ docs/extensions/sphinxgliffy.py
@@ -41,7 +41,7 @@ class Sphinxgliffy(Directive):
         # Necessary to pass X Frame Options denial
         # Image is not visible in browser, hidden by gliffy_img class
         #html_node += nodes.image(rawsource=self.block_text, **self.options)
-        html_node += nodes.image(rawsource=self.options['uri'])
+        html_node += nodes.image(rawsource=reference)
         
         return [html_node]
 
